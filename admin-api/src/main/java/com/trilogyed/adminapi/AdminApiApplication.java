c8d57d7cca866ab8eab3f4301db0f2941d0a3967
Patricia DSouza <patsy@Patricias-MacBook-Air.local>
2019-10-09 12:34:58 -0400

    Security Config

--- admin-api/src/main/java/com/trilogyed/adminapi/AdminApiApplication.java
+++ admin-api/src/main/java/com/trilogyed/adminapi/AdminApiApplication.java
@@ -2,8 +2,12 @@ package com.trilogyed.adminapi;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cache.annotation.EnableCaching;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 
 @SpringBootApplication
+@EnableDiscoveryClient
+//@EnableCaching
 public class AdminApiApplication {
 
 	public static void main(String[] args) {
